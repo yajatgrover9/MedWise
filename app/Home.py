76514f6f3e451cc3265e9226ca6ac3152aec7896
Yajat Grover <yajatgrover@gmail.com>
2025-04-08 16:46:54 +0530

    minor fixes

--- app/Home.py
+++ app/Home.py
@@ -4,12 +4,12 @@
 
 import streamlit as st
 
-st.set_page_config(page_title="app", page_icon="🩺", layout="wide")
+st.set_page_config(page_title="MedWise", page_icon="🩺", layout="wide")
 
-st.title("Welcome to app")
+st.title("Welcome to MedWise")
 st.write(
     """
-**app** is a medical assistant tool designed to help you with the following features:
+**MedWise** is a medical assistant tool designed to help you with the following features:
 - **Symptom Checker**: Enter symptoms and get a potential diagnosis.
 - **Health Report Summarizer**: Upload a blood report (PDF) to analyze potential conditions.
 """
